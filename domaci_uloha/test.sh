6bf3419ff0940746f94743839cce1f802bd9f331
drneko17 <dr.neko17@gmail.com>
2020-10-27 05:01:05 +0100

    add

--- domaci_uloha/test.sh
+++ domaci_uloha/test.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
-
 for X in {0000..0100} ; do
     if [ ! -e samples/${X}_in.txt ] ; then
-        echo "All ok"
+        echo "POGCHAMP"
         exit 0
     fi
     echo "Testing: samples/${X}_in.txt"
     ./uloha12 < samples/${X}_in.txt > /tmp/out.txt
     if ! diff samples/${X}_out.txt /tmp/out.txt ; then
-        echo "Mismatch samples/${X}_in.txt"
+        echo "RIP samples/${X}_in.txt"
         exit 1
     fi
 
