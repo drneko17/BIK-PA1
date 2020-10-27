eec0277d9252145b35db956943a47761001957e3
drneko17 <dr.neko17@gmail.com>
2020-10-27 05:57:46 +0100

    add

--- domaci_uloha/test.sh
+++ domaci_uloha/test.sh
@@ -1,10 +1,12 @@
+FILE=uloha12
+
 for X in {0000..0100} ; do
     if [ ! -e samples/${X}_in.txt ] ; then
         echo "POGCHAMP"
         exit 0
     fi
     echo "Testing: samples/${X}_in.txt"
-    ./uloha12 < samples/${X}_in.txt > /tmp/out.txt
+    ./$FILE < samples/${X}_in.txt > /tmp/out.txt
     if ! diff samples/${X}_out.txt /tmp/out.txt ; then
         echo "RIP samples/${X}_in.txt"
         exit 1
