87a22520aecd553ccdd937b98c40bfd62d5535ea
drneko17 <73367243+drneko17@users.noreply.github.com>
2020-10-27 14:29:44 +0100

    add

--- domaci_uloha2/uloha1.c
+++ domaci_uloha2/uloha1.c
@@ -2,11 +2,23 @@
 
 int main (void){
 
-    int k;
+    int k, cislo;
     printf("Poradi:\n");
-    scanf("&d",k);
-
-    
+    scanf("%d", &cislo);
+    int delitel = 3;
+    while (cislo > 1){
+        if (cislo % 2 == 0){
+            printf("Cislo je ctvercove\n");
+            return 0;
+        }
+        if (cislo % delitel == 0){
+            printf("%d", delitel);
+            cislo = cislo/delitel;
+        }
+        if (cislo % delitel != 0){
+            delitel = delitel + 1;
+        }
+    }
 
     return 0;
 }
